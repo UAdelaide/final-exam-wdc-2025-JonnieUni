044241d36a8f2b7b5cb0f6b5eb3df2a805269fe0
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 06:48:36 +0000

    [automatic commit] 2025-06-20 06:48:36 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -137,6 +137,7 @@ let db;
                 ((SELECT dog_id FROM Dogs WHERE name = 'Coco'), '2025-06-12 09:00:00', 40, 'Hillside Path', 'cancelled');
             `);
         }
+        
     }
 })
 
