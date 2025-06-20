44bf415cec93b0a0e379a01b2fb01a889fe14838
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 06:53:12 +0000

    [automatic commit] 2025-06-20 06:53:12 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -155,7 +155,7 @@ app.get('/api/dogs', async function (req, res) {
         `);
         res.json(rows);
     } catch (err) {
-        res.status(500).json({})
+        res.status(500).json({ error: 'Failed to fetch dogs', details: err.message })
     }
 })
 
