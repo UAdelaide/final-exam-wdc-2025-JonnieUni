b7de39726e0c98354792c7251b76f7ca17c50105
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 11:37:28 +0000

    [automatic commit] 2025-06-20 11:37:28 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -139,7 +139,7 @@ let db;
             await db.query(`
                 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating)
                 VALUES
-                (1, (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username =))
+                (1, (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123))
             `);
         }
     } catch (err) {
