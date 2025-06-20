aa8be9493c8c1d232d067a1856888595fbd61535
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 06:34:22 +0000

    [automatic commit] 2025-06-20 06:34:22 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -105,8 +105,10 @@ let db;
         `);
 
         // Inserting data onily if Users table is empty
-        var [userRows] = await db.execute('SELECT COUNT(*) AS count FROM Users')
-
+        var [userRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        if (userRows[0].count === 0) {
+            await
+        }
     }
 })
 
