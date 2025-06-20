c81735fcd4ba8d2cb2a89823f6c4789425084c6e
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 06:52:09 +0000

    [automatic commit] 2025-06-20 06:52:08 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -149,7 +149,9 @@ let db;
 app.get('/api/dogs', async function (req, res) {
     try {
         var[rows] = await db.query(`
-            
+            SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+            FROM Dogs
+            JOIN Users ON Dogs.owner_id = Users.user_id
         `);
     }
 })
