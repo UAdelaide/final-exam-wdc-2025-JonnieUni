f874286f33c78c49abc75a1b0c0a3671abd83411
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-20 06:47:34 +0000

    [automatic commit] 2025-06-20 06:47:33 UTC 58.174.84.29

--- part1/app.js
+++ part1/app.js
@@ -118,13 +118,13 @@ let db;
             `);
 
             await db.query(`
-                INSERT INTO Users (username, email, password_hash, role)
-                VALUES
-                ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-                ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-                ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-                ('davidsmith', 'david@example.com', 'hashed999', 'walker'),
-                ('jon123', 'jon@example.com', 'hashed000', 'owner');
+INSERT INTO Dogs (owner_id, name, size)
+VALUES
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Rocky', 'large'),
+((SELECT user_id FROM Users WHERE username = 'davidsmith'), 'Luna', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'jon123'), 'Coco', 'small');
             `);
         }
     }
