a7a43764814da437a2c5d6fca19d6cd53585300f
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-21 02:44:29 +0000

    [automatic commit] 2025-06-21 02:44:28 UTC 58.174.84.29

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login (dummy version) // changes made here
+// POST login (dummy version) // Changes made here
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
