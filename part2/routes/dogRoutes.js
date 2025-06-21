425a3276aa501ad7498a198fc657470cae6d295a
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-21 05:19:21 +0000

    [automatic commit] 2025-06-21 05:19:21 UTC 58.174.84.29

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
 
 // a GET /api/dogs to list all dogs with owner info
 router.get('/dogs', async (req, res) => {
--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -79,18 +79,4 @@ router.get('/my-dogs', async (req, res) => {
   }
 });
 
-// a GET /api/dogs to list all dogs with owner info
-router.get('/dogs', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
-      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
-      FROM Dogs
-      JOIN Users ON Dogs.owner_id = Users.user_id
-    `);
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs', details: err.message });
-  }
-});
-
 module.exports = router;
