0ee56a7cee3fe062cbda6a9b24c322d0429cb869
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-21 04:50:12 +0000

    [automatic commit] 2025-06-21 04:50:11 UTC 58.174.84.29

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -87,7 +87,7 @@ router.get('/dogs', async (req, res) => {
     );
     res.json(rows);
   } catch (err) {
-    res.status(500).json()
+    res.status(500).json({ error: 'Failed to fetch dogs', details: err.message });
   }
 })
 
