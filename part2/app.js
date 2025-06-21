66d72f469da4dde3357caab1e0fd037a206df58e
Jonathon Sadler <a1803088@adelaide.edu.au>
2025-06-21 02:43:23 +0000

    [automatic commit] 2025-06-21 02:43:23 UTC 58.174.84.29

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+// Added this line - imported express-session middleware
 const session = require('express-session');
 require('dotenv').config();
 
